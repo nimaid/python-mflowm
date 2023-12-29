199bb1fef6d01c08c1639c2f5a35e6260b87ff94
Ella Jameson <ellagjameson@gmail.com>
2023-12-29 06:32:24 -0700

    Update helpers.py

--- src/mflowm/helpers.py
+++ src/mflowm/helpers.py
@@ -1,10 +1,17 @@
 import os
 
 
-# Makes sure a string represents a valid, existing file
-# This can be used with argparse as a valid argument type
-def file_path(string):
-    if os.path.isfile(string):
-        return string
+def file_path(filename: str) -> str:
+    """Makes sure a string represents a valid, existing file, this can be used with argparse as a valid argument type.
+
+    :param str filename: The filename to test
+
+    :raises FileNotFoundError: When the file is not found.
+
+    :return: The filename, unmodified.
+    :rtype: str
+    """
+    if os.path.isfile(filename):
+        return filename
     else:
-        raise FileNotFoundError(string)
+        raise FileNotFoundError(filename)
