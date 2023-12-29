ad25a4eb981e6c3ddb538833af52512e5b1d65ba
Ella Jameson <ellagjameson@gmail.com>
2023-12-29 05:54:45 -0700

    Update __init__.py

--- src/mflowm/__init__.py
+++ src/mflowm/__init__.py
@@ -1,6 +1,6 @@
 """MotionFlowMulti - A multi-resolution colorful motion detection video filter."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from mflowm.files import VideoReader
 from mflowm.flow import MotionFlowMulti, CompositeMode
